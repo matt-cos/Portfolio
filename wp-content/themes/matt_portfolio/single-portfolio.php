eac1381aac98b72f8259d77889ef4b55d6dc2642
Matt Cosentino <matt@learnedmedia.com>
2018-05-30 21:59:52 -0400

    tightened up single portfolio item template

--- wp-content/themes/matt_portfolio/single-portfolio.php
+++ wp-content/themes/matt_portfolio/single-portfolio.php
@@ -27,60 +27,65 @@ get_header();
 					</div>
 				</div>
 
+				<?php if( get_field('project_1_image') ): ?>
+					<?php $image1 = get_field('project_1_image'); ?>
+
+					<div class="row project">
+						<div class="column column-25 project__column">
+							<p class="project__info"><?php the_field('project_1_info'); ?></p>
+						</div>
+						<div class="column column-75">
+							<img src="<?php echo $image1['url']; ?>" alt="<?php echo $image1['alt']; ?>" class="project__image">
+						</div>
+					</div>
+				<?php endif; ?>
+
+				<?php if( get_field('project_2_image') ): ?>
+					<?php $image2 = get_field('project_2_image'); ?>
+
+					<div class="row project">
+						<div class="column column-25 project__column">
+							<p class="project__info"><?php the_field('project_2_info'); ?></p>
+						</div>
+						<div class="column column-75">
+							<img src="<?php echo $image2['url']; ?>" alt="<?php echo $image2['alt']; ?>" class="project__image">
+						</div>
+					</div>
+				<?php endif; ?>
+
+				<?php if( get_field('project_3_image') ): ?>
+					<?php $image3 = get_field('project_3_image'); ?>
+
+					<div class="row project">
+						<div class="column column-25 project__column">
+							<p class="project__info"><?php the_field('project_3_info'); ?></p>
+						</div>
+						<div class="column column-75">
+							<img src="<?php echo $image3['url']; ?>" alt="<?php echo $image3['alt']; ?>" class="project__image">
+						</div>
+					</div>
+				<?php endif; ?>
+
+				<?php if( get_field('project_4_image') ): ?>
+					<?php $image4 = get_field('project_4_image'); ?>
+
+					<div class="row project">
+						<div class="column column-25 project__column">
+							<p class="project__info"><?php the_field('project_4_info'); ?></p>
+						</div>
+						<div class="column column-75">
+							<img src="<?php echo $image4['url']; ?>" alt="<?php echo $image4['alt']; ?>" class="project__image">
+						</div>
+					</div>
+				<?php endif; ?>
+
+				
 				<div class="row">
 					<div class="column column-25">
-						<?php
-							while ( have_posts() ) : the_post();
 
-								the_content();
-
-						?>
 					</div>
 					<div class="column column-75">
-						<?php
 
-								matt_portfolio_post_thumbnail();
-
-							endwhile; // End of the loop.
-						?>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="column column-25">
-						<?php
-							while ( have_posts() ) : the_post();
-
-								the_content();
-
-						?>
-					</div>
-					<div class="column column-75">
-						<?php
-
-								matt_portfolio_post_thumbnail();
-
-							endwhile; // End of the loop.
-						?>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="column column-25">
-						<?php
-							while ( have_posts() ) : the_post();
-
-								the_content();
-
-						?>
-					</div>
-					<div class="column column-75">
-						<?php
-
-								matt_portfolio_post_thumbnail();
-
-							endwhile; // End of the loop.
-						?>
 					</div>
 				</div>
 
